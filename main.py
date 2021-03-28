f821c60a5a288624113eaf928026f6716acb7de5
Grant Stahr <76236444+InsulatorGMan@users.noreply.github.com>
2021-03-28 10:48:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$'):
+        await message.channel.send('Hello!')
+   
+
+// put your discord bots token here
+client.run('') 
